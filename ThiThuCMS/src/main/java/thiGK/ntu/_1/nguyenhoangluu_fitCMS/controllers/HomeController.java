31db237e534f4886b7072822ed015e68de4d0cf2
LuuHocGioi <nguyenluwu@gmail.com>
2026-03-11 15:41:48 +0700

    .

--- ThiThuCMS/src/main/java/thiGK/ntu/_1/nguyenhoangluu_fitCMS/controllers/HomeController.java
+++ ThiThuCMS/src/main/java/thiGK/ntu/_1/nguyenhoangluu_fitCMS/controllers/HomeController.java
@@ -1,12 +1,35 @@
 package thiGK.ntu._1.nguyenhoangluu_fitCMS.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import thiGK.ntu._1.nguyenhoangluu_fitCMS.models.Page;
+import thiGK.ntu._1.nguyenhoangluu_fitCMS.models.Post;
+
 @Controller
+
 public class HomeController {
-	@GetMapping("/")
-	public String home() {
-		return "index";
+//	@GetMapping("/index")
+//	public String home() {
+//		return "index";
+//	}
+	//Danh sach page, post
+	ArrayList<Page> dsTrang = new ArrayList<Page>();
+	ArrayList<Post> dsBaiViet = new ArrayList<Post>();
+
+	public HomeController(){
+		dsTrang.add(new Page () );
+		dsTrang.add(new Page () );
+		dsTrang.add(new Page () );
 	}
+	@GetMapping("/page/all")
+	public String getAllTrang(ModelMap m) {
+		m.addAttribute("lstPages","dsTrang");
+		
+		return "allpage";
+	}
+	//Action
 }
---
+++ ThiThuCMS/src/main/java/thiGK/ntu/_1/nguyenhoangluu_fitCMS/models/Page.java
@@ -0,0 +1,5 @@
+package thiGK.ntu._1.nguyenhoangluu_fitCMS.models;
+
+public class Page {
+
+}
