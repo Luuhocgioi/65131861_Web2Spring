6000eed32a143fe51f7a635221eb32e79e7d6f12
LuuHocGioi <nguyenluwu@gmail.com>
2026-03-11 16:21:16 +0700

    .

--- ThiThuCMS/src/main/java/thiGK/ntu/_1/nguyenhoangluu_fitCMS/controllers/HomeController.java
+++ ThiThuCMS/src/main/java/thiGK/ntu/_1/nguyenhoangluu_fitCMS/controllers/HomeController.java
@@ -21,13 +21,13 @@ public class HomeController {
 	ArrayList<Post> dsBaiViet = new ArrayList<Post>();
 
 	public HomeController(){
-		dsTrang.add(new Page () );
-		dsTrang.add(new Page () );
-		dsTrang.add(new Page () );
+		dsTrang.add(new Page(1, "Giới thiệu", "intro, ntu", "Nội dung giới thiệu...", 0));
+		dsTrang.add(new Page(2, "Liên hệ", "contact", "Thông tin liên hệ...", 1));
+		dsTrang.add(new Page(3, "Chính sách", "Terms", "Chính sách của chúng tôi", 1));
 	}
 	@GetMapping("/page/all")
 	public String getAllTrang(ModelMap m) {
-		m.addAttribute("lstPages","dsTrang");
+		m.addAttribute("lstPages",dsTrang);
 		
 		return "allpage";
 	}
--- ThiThuCMS/src/main/java/thiGK/ntu/_1/nguyenhoangluu_fitCMS/models/Page.java
+++ ThiThuCMS/src/main/java/thiGK/ntu/_1/nguyenhoangluu_fitCMS/models/Page.java
@@ -1,5 +1,25 @@
 package thiGK.ntu._1.nguyenhoangluu_fitCMS.models;
 
 public class Page {
+    private int id;
+    private String pageName;
+    private String keyword;
+    private String content;
+    private int parentPageId;
 
+    // PHẢI CÓ: Constructor để tạo đối tượng
+    public Page(int id, String pageName, String keyword, String content, int parentPageId) {
+        this.id = id;
+        this.pageName = pageName;
+        this.keyword = keyword;
+        this.content = content;
+        this.parentPageId = parentPageId;
+    }
+
+    // PHẢI CÓ: Các Getter để Thymeleaf lấy được dữ liệu
+    public int getId() { return id; }
+    public String getPageName() { return pageName; }
+    public String getKeyword() { return keyword; }
+    public String getContent() { return content; }
+    public int getParentPageId() { return parentPageId; }
 }
