3df9db8a032ba532a5fa177fada5cee5b98fff81
LuuHocGioi <nguyenluwu@gmail.com>
2026-03-13 22:42:19 +0700

    .

--- B2_TruyenDuLieuSangView/src/main/java/Baitap/ntu65131861/nguyenhoangluu/controller/HomeController.java
+++ B2_TruyenDuLieuSangView/src/main/java/Baitap/ntu65131861/nguyenhoangluu/controller/HomeController.java
@@ -1,9 +1,10 @@
 package Baitap.ntu65131861.nguyenhoangluu.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class HomeController {
 	
 	@GetMapping("/info")
