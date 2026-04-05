d22afffc2c439ea4be0d58fbdd9adf9462d908a4
LuuHocGioi <nguyenluwu@gmail.com>
2026-04-05 12:29:54 +0700

    .

---
+++ QL-Session/src/main/java/Baitap/ntu65131861/nguyenhoangluu/QL_Session/QlSessionApplication.java
@@ -0,0 +1,13 @@
+package Baitap.ntu65131861.nguyenhoangluu.QL_Session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlSessionApplication.class, args);
+	}
+
+}
